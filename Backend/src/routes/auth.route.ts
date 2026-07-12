41eb8997fc81a1cdf25f48ca68caaf7a52fe7862
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-07-12 16:47:42 +0530

    add authentication and profile routes to auth router

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { ConnectLeetcode, getMe, login, logout, signup } from "../controllers/profile.controller";
 import { loginMiddleware } from "../middlewares/loginMiddleware";
+import { ConnectLeetcode, getMe, login, logout, signup } from "../controllers/profile.controller";
 
 const router = express.Router();
 
 router.post("/signup", signup );
-router.post("/login", login );
+router.post("/login", login);
 router.post("/logout", logout);
 router.post("/connectLeetCode",ConnectLeetcode);
 router.get('/me',loginMiddleware,getMe);
